d91300fb18151fb85211629b8f98d15427fe2289
Lara Ferreira <73527417+178LaPata@users.noreply.github.com>
2023-06-05 23:14:48 +0100

    ACABOU

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -1,30 +1,28 @@
 use autovrumvrum;
 
 -- -------------------------  QUERY 1  -------------------------
-DROP PROCEDURE IF EXISTS carros_disponiveis;
+DROP VIEW IF EXISTS viewCarrosDisponiveis;
 
 DELIMITER //
-CREATE PROCEDURE carros_disponiveis ()
-	BEGIN
-		SELECT * FROM Carro
-		WHERE Carro.ID NOT IN (SELECT Venda.Carro_ID FROM Venda WHERE Venda.Carro_ID);
-	END //
+CREATE VIEW viewCarrosDisponiveis AS
+    SELECT Carro.Marca, Carro.Modelo, Carro.Ano, Carro.Kilometros, Carro.Cilindrada, Carro.Combustivel, Carro.Preco, Carro.Estado
+    FROM Carro
+    WHERE Carro.ID NOT IN (SELECT Venda.Carro_ID FROM Venda WHERE Venda.Carro_ID);
 DELIMITER //
 
-CALL carros_disponiveis();
+SELECT * FROM viewCarrosDisponiveis;
 
 -- -------------------------  QUERY 2  -------------------------
-DROP PROCEDURE IF EXISTS carros_vendidos;
+DROP VIEW IF EXISTS viewCarrosVendidos;
 
 DELIMITER //
-CREATE PROCEDURE carros_vendidos ()
-    BEGIN
-        SELECT * FROM Carro
+    CREATE VIEW viewCarrosVendidos AS
+        SELECT Carro.Marca, Carro.Modelo, Carro.Ano, Carro.Kilometros, Carro.Cilindrada, Carro.Combustivel, Carro.Preco, Carro.Estado
+        FROM Carro
         WHERE Carro.ID IN (SELECT Venda.Carro_ID FROM Venda WHERE Venda.Carro_ID);
-    END //
 DELIMITER //
 
-CALL carros_vendidos();
+SELECT * FROM viewCarrosVendidos;
 
 -- -------------------------  QUERY 3  -------------------------
 DROP PROCEDURE IF EXISTS funcionarios_disponiveis;
@@ -32,23 +30,23 @@ DROP PROCEDURE IF EXISTS funcionarios_disponiveis;
 DELIMITER //
 CREATE PROCEDURE funcionarios_disponiveis ()
     BEGIN
-        SELECT * FROM Funcionario;
+        SELECT funcionario.Nome, funcionario.Genero, funcionario.Data_de_Nascimento, funcionario.NIF, funcionario.Tipo
+        FROM Funcionario;
     END //
 DELIMITER //
 
 CALL funcionarios_disponiveis();
 
 -- -------------------------  QUERY 4  -------------------------
-DROP PROCEDURE IF EXISTS clientes_stand;
+DROP VIEW IF EXISTS viewClientes;
 
 DELIMITER //
-CREATE PROCEDURE clientes_stand ()
-    BEGIN
-        SELECT * FROM Cliente;
-    END //
+CREATE VIEW viewClientes AS
+    SELECT Cliente.Nome, Cliente.Genero, Cliente.Data_de_Nascimento, Cliente.NIF, Cliente.Morada
+    FROM Cliente;
 DELIMITER //
 
-CALL clientes_stand();
+SELECT * FROM viewClientes;
 
 -- -------------------------  QUERY 5  -------------------------
 DROP PROCEDURE IF EXISTS carros_disponiveis_por_marca;
@@ -56,13 +54,14 @@ DROP PROCEDURE IF EXISTS carros_disponiveis_por_marca;
 DELIMITER //
 CREATE PROCEDURE carros_disponiveis_por_marca (IN marca VARCHAR(50))
     BEGIN
-        SELECT * FROM Carro
+        SELECT carro.Marca, carro.Modelo, carro.Ano, carro.Kilometros, carro.Cilindrada, carro.Combustivel, carro.Preco, carro.Estado
+        FROM Carro
         WHERE Carro.ID NOT IN (SELECT Venda.Carro_ID FROM Venda WHERE Venda.Carro_ID)
         AND Carro.Marca = marca;
     END //
 DELIMITER //
 
-CALL carros_disponiveis_por_marca('xiaomi');
+CALL carros_disponiveis_por_marca('bmw');
 
 -- -------------------------  QUERY 6  -------------------------
 DROP PROCEDURE IF EXISTS historico_compras_cliente;
@@ -70,7 +69,10 @@ DROP PROCEDURE IF EXISTS historico_compras_cliente;
 DELIMITER //
 CREATE PROCEDURE historico_compras_cliente (IN cliente_id INT)
     BEGIN
-        SELECT * FROM Venda
+        SELECT cliente.Nome , carro.Marca, carro.Modelo, carro.Ano, carro.Kilometros, carro.Cilindrada, carro.Combustivel, carro.Preco, carro.Estado, venda.Data_Venda, venda.Preco
+        FROM Venda
+        INNER JOIN Carro ON Carro.ID = Venda.Carro_ID
+        INNER JOIN Cliente ON Cliente.ID = Venda.Cliente_ID
         WHERE Venda.Cliente_ID = cliente_id;
     END //
 DELIMITER //
@@ -83,7 +85,12 @@ DROP PROCEDURE IF EXISTS historico_testDrives;
 DELIMITER //
 CREATE PROCEDURE historico_testDrives(IN cliente_id INT)
     BEGIN
-        SELECT * FROM TestDrive
+        SELECT cliente.Nome, carro.Marca, carro.Modelo, carro.Ano, carro.Kilometros,
+               carro.Cilindrada, carro.Combustivel, testDrive.Data_Ocorrencia, funcionario.Nome AS 'Funcionario'
+        FROM TestDrive
+        INNER JOIN Carro ON Carro.ID = TestDrive.Carro_ID
+        INNER JOIN Cliente ON Cliente.ID = TestDrive.Cliente_ID
+        INNER JOIN Funcionario ON Funcionario.ID = TestDrive.Funcionario_ID
         WHERE TestDrive.Cliente_ID = cliente_id;
     END //
 DELIMITER //
@@ -91,37 +98,35 @@ DELIMITER //
 CALL historico_testDrives(1);
 
 -- -------------------------  QUERY 8  -------------------------
-DROP PROCEDURE IF EXISTS carrosMaisVendidosMarca;
+DROP PROCEDURE IF EXISTS ObterMarcasMaisVendidas;
 
 DELIMITER //
-CREATE PROCEDURE carrosMaisVendidosMarca()
-    BEGIN
-        SELECT Carro.Marca, COUNT(Venda.Carro_ID) AS 'Vendas'
-        FROM Carro, Venda
-        WHERE Carro.ID = Venda.Carro_ID
-        GROUP BY Carro.ID
-        ORDER BY Vendas DESC
-        LIMIT 5;
-    END //
+CREATE PROCEDURE ObterMarcasMaisVendidas()
+BEGIN
+    SELECT Carro.Marca, COUNT(*) AS Quantidade
+    FROM Carro
+    INNER JOIN Venda ON carro.ID = Venda.Carro_ID
+    GROUP BY Carro.Marca
+    ORDER BY Quantidade DESC, Carro.Marca;
+END //
 DELIMITER //
 
-CALL carrosMaisVendidosMarca();
+CALL ObterMarcasMaisVendidas();
 
 -- -------------------------  QUERY 9  -------------------------
 DROP PROCEDURE IF EXISTS receitasMensais;
 
 DELIMITER //
-CREATE PROCEDURE receitasMensais(IN mes INT)
+CREATE PROCEDURE receitasMensais()
     BEGIN
-        DECLARE receita DECIMAL(10, 2);
-        SELECT SUM(Venda.Preco) INTO receita
+        SELECT MONTH(Venda.Data_Venda) AS Mes, SUM(Venda.Preco) AS Receita
         FROM Venda
-        WHERE MONTH(Venda.Data_Venda) = mes;
-        SELECT receita AS 'Receita';
+        GROUP BY Mes
+        ORDER BY Receita DESC;
     END //
 DELIMITER //
 
-CALL receitasMensais(3);
+CALL receitasMensais();
 
 -- -------------------------  QUERY 10  -------------------------
 DROP PROCEDURE IF EXISTS fornecores_disponiveis;
@@ -141,25 +146,14 @@ DROP PROCEDURE IF EXISTS comprasFornecedor;
 DELIMITER //
 CREATE PROCEDURE comprasFornecedor(IN fornecedor_id INT)
     BEGIN
-        SELECT * FROM Compra
+        SELECT Carro.Marca, Carro.Ano, Carro.Modelo, Carro.Cilindrada, Carro.Combustivel, Carro.Preco, Compra.Data_Compra, Fornecedor.Nome AS 'Fornecedor', Funcionario.Nome AS 'Funcionario'
+        FROM Compra
+        INNER JOIN Carro ON Carro.ID = Compra.Carro_ID
+        INNER JOIN Fornecedor ON Fornecedor.ID = Compra.Fornecedor_ID
+        INNER JOIN Funcionario ON Funcionario.ID = Compra.Funcionario_ID
         WHERE Compra.Fornecedor_ID = fornecedor_id;
     END //
 DELIMITER //
 
 CALL comprasFornecedor(1);
-
--- -------------------------  QUERY 12  -------------------------
-DROP VIEW IF EXISTS top10CarrosMaisVendidos;
-
-CREATE VIEW top10CarrosMaisVendidos AS
-SELECT Carro.Marca, Carro.Modelo, COUNT(Venda.Carro_ID) AS 'Vendas'
-FROM Carro, Venda
-WHERE Carro.ID = Venda.Carro_ID
-GROUP BY Carro.ID
-ORDER BY Vendas DESC
-LIMIT 10;
-
-SELECT * FROM top10CarrosMaisVendidos;
-
-
 
